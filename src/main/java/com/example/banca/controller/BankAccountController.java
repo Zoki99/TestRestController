5a73075948db2fd4e31f759991375c4cf927bbfd
Zoran <zoran.micevski@bitsrl.net>
2022-03-08 10:43:22 +0100

    Test Controller

--- src/main/java/com/example/banca/controller/BankAccountController.java
+++ src/main/java/com/example/banca/controller/BankAccountController.java
@@ -2,41 +2,30 @@ package com.example.banca.controller;
 import com.example.banca.model.ClientModel.ClientBalance;
 import com.example.banca.model.TransactionsModel.TransactionsRoot;
 import com.example.banca.model.TransactionsModel.TranscationsList;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import com.google.gson.JsonParser;
 
 
 @RestController
 public class BankAccountController {
     Logger logger = LoggerFactory.getLogger(BankAccountController.class);
+    private final String URL = "https://sandbox.platfr.io";
 
     @GetMapping("/getBalance/{accountId}")
-    public BigDecimal getBalance(@PathVariable long accountId) {
-
+    public BigDecimal getBalance(HttpEntity<String> httpEntity, @PathVariable long accountId) {
         // request url
-        String url = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+accountId+"/balance";
+        String url = URL+"/api/gbs/banking/v4.0/accounts/"+accountId+"/balance";
         BigDecimal balance = null;
+
         // create an instance of RestTemplate
         RestTemplate restTemplate = new RestTemplate();
-
-        // create headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Auth-Schema", "S2S");
-        headers.set("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-
-        // build the request
-        HttpEntity<String> request = new HttpEntity<>(headers);
+        HttpEntity<String> request = new HttpEntity<String>(httpEntity.getHeaders());
 
         // make an HTTP GET request with headers
         ResponseEntity<ClientBalance> response = restTemplate.exchange(url, HttpMethod.GET, request, ClientBalance.class);
@@ -56,29 +45,22 @@ public class BankAccountController {
 
 
     @GetMapping("/getTransactions/{accountId}/{from}/{to}")
-    public ArrayList<TranscationsList> getTransactions(@PathVariable long accountId, @PathVariable String from, @PathVariable String to) {
-
+    public ArrayList<TranscationsList> getTransactions(HttpEntity<String> httpEntity, @PathVariable long accountId, @PathVariable String from, @PathVariable String to) {
         // request url
-        String url = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+accountId+"/transactions?fromAccountingDate="+from+"&toAccountingDate="+to+"/balance";
+        String url = URL+"/api/gbs/banking/v4.0/accounts/"+accountId+"/transactions?fromAccountingDate="+from+"&toAccountingDate="+to+"/balance";
 
         // create an instance of RestTemplate
         RestTemplate restTemplate = new RestTemplate();
-
-        // create headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Auth-Schema", "S2S");
-        headers.set("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-
-        // build the request
-        HttpEntity<String> request = new HttpEntity<>(headers);
+        HttpEntity<String> request = new HttpEntity<String>(httpEntity.getHeaders());
 
         // make an HTTP GET request with headers
         ResponseEntity<TransactionsRoot> response = restTemplate.exchange(url, HttpMethod.GET, request, TransactionsRoot.class);
         ArrayList<TranscationsList> list = new ArrayList<TranscationsList>();
+
         // check response
         if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
             list = response.getBody().getPayload().getList();
-           // transactions = response.getBody().getPayload().getList();
+            // transactions = response.getBody().getPayload().getList();
             logger.info(response.getStatusCode() + " Request Successful.");
             logger.info(list.toString());
         } else {
@@ -90,10 +72,9 @@ public class BankAccountController {
 
     @RequestMapping(value = "/money-transfers/{accountId}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public void moneyTransfer(HttpEntity<String> httpEntity, @PathVariable long accountId) {
-        String json = httpEntity.getBody();
         RestTemplate restTemplate = new RestTemplate();
-        String url  = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+accountId+"/payments/money-transfers";
-        HttpEntity<String> entity = new HttpEntity<String>(json, httpEntity.getHeaders());
+        String url  = URL+"/api/gbs/banking/v4.0/accounts/"+accountId+"/payments/money-transfers";
+        HttpEntity<String> entity = new HttpEntity<String>(httpEntity.getBody(), httpEntity.getHeaders());
         ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
         logger.info(response.toString());
     }
@@ -105,7 +86,6 @@ public class BankAccountController {
     public ResponseEntity<String> handleHttpClientErrorException(HttpClientErrorException exception) {
         logger.error("Request Failed");
         logger.error(ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage()).toString());
-
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
--- src/test/java/com/example/banca/BancaApplicationTests.java
+++ src/test/java/com/example/banca/BancaApplicationTests.java
@@ -9,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,50 +20,10 @@ import java.math.BigDecimal;
 
 @SpringBootTest
 class BancaApplicationTests {
-	private long accountId;
+	private long accountId = 14537780;
 	private String from = "2020-03-01";
 	private String to = "2020-04-01";
 
-	String body = "{\n" +
-			"  \"creditor\": {\n" +
-			"    \"name\": \"John Doe\",\n" +
-			"    \"account\": {\n" +
-			"      \"accountCode\": \"IT23A0336844430152923804660\",\n" +
-			"      \"bicCode\": \"SELBIT2BXXX\"\n" +
-			"    },\n" +
-			"    \"address\": {\n" +
-			"      \"address\": null,\n" +
-			"      \"city\": null,\n" +
-			"      \"countryCode\": null\n" +
-			"    }\n" +
-			"  },\n" +
-			"  \"executionDate\": \"2019-04-01\",\n" +
-			"  \"uri\": \"REMITTANCE_INFORMATION\",\n" +
-			"  \"description\": \"Payment invoice 75/2017\",\n" +
-			"  \"amount\": 800,\n" +
-			"  \"currency\": \"EUR\",\n" +
-			"  \"isUrgent\": false,\n" +
-			"  \"isInstant\": false,\n" +
-			"  \"feeType\": \"SHA\",\n" +
-			"  \"feeAccountId\": \"45685475\",\n" +
-			"  \"taxRelief\": {\n" +
-			"    \"taxReliefId\": \"L449\",\n" +
-			"    \"isCondoUpgrade\": false,\n" +
-			"    \"creditorFiscalCode\": \"56258745832\",\n" +
-			"    \"beneficiaryType\": \"NATURAL_PERSON\",\n" +
-			"    \"naturalPersonBeneficiary\": {\n" +
-			"      \"fiscalCode1\": \"MRLFNC81L04A859L\",\n" +
-			"      \"fiscalCode2\": null,\n" +
-			"      \"fiscalCode3\": null,\n" +
-			"      \"fiscalCode4\": null,\n" +
-			"      \"fiscalCode5\": null\n" +
-			"    },\n" +
-			"    \"legalPersonBeneficiary\": {\n" +
-			"      \"fiscalCode\": null,\n" +
-			"      \"legalRepresentativeFiscalCode\": null\n" +
-			"    }\n" +
-			"  }\n" +
-			"}";
 	@Autowired
 	private BankAccountController controller;
 
@@ -69,43 +31,16 @@ class BancaApplicationTests {
 
 	@Test
 	void getBalanceTest(){
-		assertThat(controller.getBalance(accountId)).isNotNull();
-		assertThat(controller.getBalance(accountId).toString()).isEqualTo("8.40");
-
-	}
-
-	@Test
-	void getTransactionsTest() {
-		assertThat(controller.getTransactions(accountId, from, to)).isNotNull();
-	}
-
-
-	@Test
-	void moneyTransfer() {
-		assertThat(controller.getTransactions(accountId, from, to)).isNotNull();
+		// create headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Auth-Schema", "S2S");
+		headers.set("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		assertThat(controller.getBalance(request, accountId)).isNotNull();
+		assertThat(controller.getBalance(request, accountId).toString()).isEqualTo("8.37");
 
 	}
 
 
 
-	private MockMvc mvc;
-
-	@Test
-	public void createEmployeeAPI() throws Exception
-	{
-		mvc.perform( MockMvcRequestBuilders
-				.post("/money-transfers/"+accountId)
-				.content(body)
-				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().is2xxSuccessful());
-	}
-
-//	public static String asJsonString(final Object obj) {
-//		try {
-//			return new ObjectMapper().writeValueAsString(obj);
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		}
-//	}
-
-	}
+}
